b790a748f10f84d342b4eceb5300240560a7e8d1
amitkroutthedev <akrout455@gmail.com>
2022-10-28 16:19:17 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+//#3e3f3f
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      gray: "#696969",
+      "grey-light": "#d3dce6",
+      black: "#090908",
+      "light-dark": "#1f1e1f",
+      violet: " #cc33ff",
+      white: "#faf8f7",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
